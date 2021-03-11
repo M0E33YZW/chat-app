1d34c27cd62fe9496d799f0dae7ca78a7968ee1e
nyanmy <nyanmyth100@gmail.com>
2021-03-11 21:41:43 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-<<<<<<< Updated upstream
-
-  validates :content, presence: true
-end
-=======
   has_one_attached :image
 
   validates :content, presence: true
 end
->>>>>>> Stashed changes
