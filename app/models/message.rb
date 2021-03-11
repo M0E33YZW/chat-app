67cd3f49ed00a12c5a84937c4b1cd3b2cd700ac4
nyanmy <nyanmyth100@gmail.com>
2021-03-11 21:29:41 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+<<<<<<< Updated upstream
 
   validates :content, presence: true
 end
+=======
+  has_one_attached :image
+
+  validates :content, presence: true
+end
+>>>>>>> Stashed changes
